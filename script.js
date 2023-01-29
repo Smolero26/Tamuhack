b7c8dc426cbc78424eb2553df7dfd1777d2167e9
Smolero26 <118874345+Smolero26@users.noreply.github.com>
2023-01-28 21:40:27 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 // DOM
-const swiper = document.querySelector('#swipe');
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
 
+// constants
 const urls = [
-    'https://design.gs.com/downloads/Goldman_Sachs_Blue_Box.png',
-    'https://1000logos.net/wp-content/uploads/2016/10/American-Airlines-Logo.png',
-    'https://tamuhack.com/static/th-2023/assets/bp.png',
-    'https://images.law.com/contrib/content/uploads/sites/401/2022/02/USAA-Logo-767x633.jpg',
-    'https://image.cnbcfm.com/api/v1/image/106821423-1610125493528-Untitled-5.jpg?v=1610125520'
+  'https://source.unsplash.com/random/1000x1000/?sky',
+  'https://source.unsplash.com/random/1000x1000/?landscape',
+  'https://source.unsplash.com/random/1000x1000/?ocean',
+  'https://source.unsplash.com/random/1000x1000/?moutain',
+  'https://source.unsplash.com/random/1000x1000/?forest'
 ];
 
+// variables
 let cardCount = 0;
 
-function appendNewCard(){
-    const card = new Card({
-        imageUrl:urls[cardCount % 5]
-    });
-    card.element.style.setProperty('--i', cardCount % 5);
-    swiper.append(card.element);
-    cardCount++;
+// functions
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 5],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    }
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    card.style.setProperty('--i', index);
+  });
 }
 
+// first 5 cards
 for (let i = 0; i < 5; i++) {
-    appendNewCard();
+  appendNewCard();
 }
