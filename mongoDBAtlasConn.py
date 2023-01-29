c8a37f56edcdbdbf16cf09d0eabfad4e72d4d6d0
PedroLHernandez <123842238+PedroLHernandez@users.noreply.github.com>
2023-01-28 19:56:19 -0600

    connecting pymongo to DB

---
+++ mongoDBAtlasConn.py
@@ -0,0 +1,8 @@
+import pymongo
+
+#client = pymongo.MongoClient(mongodb+srv://tamuhack:tamu012823@cluster0.6bs3nry.mongodb.net/?retryWrites=true&w=majority)
+
+client = pymongo.MongoClient("mongodb+srv://tamuhack:tamu012823@cluster0.6bs3nry.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+
